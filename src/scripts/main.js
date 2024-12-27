05813c0d4c3fb4addafe8a7e485096365588d7a9
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2024-12-27 05:47:43 -0300

    add character section with draggable list and info display

---
+++ src/scripts/main.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const faces = document.querySelectorAll('[data-character-face]')
+    const infos = document.querySelectorAll('[data-character-id]');
+
+    const list = document.querySelector('.characters__main__list');
+    let isDragging = false;
+    let startY;
+    let scrollTop;
+
+    // Efeito de Drag
+    const handleScrollLoop = () => {
+        const maxScrollTop = list.scrollHeight - list.clientHeight;
+        
+        if (list.scrollTop <= 0) {
+            list.scrollTop = maxScrollTop;
+        } else if (list.scrollTop >= maxScrollTop) {
+            list.scrollTop = 0;
+        }
+    };
+
+    list.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startY = e.pageY - list.offsetTop;
+        scrollTop = list.scrollTop;
+        list.style.cursor = 'grabbing';
+    });
+
+    list.addEventListener('mouseleave', () => {
+        isDragging = false;
+        list.style.cursor = 'grab';
+    });
+
+    list.addEventListener('mouseup', () => {
+        isDragging = false;
+        list.style.cursor = 'grab';
+    });
+
+    list.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        
+        const y = e.pageY - list.offsetTop;
+        const walk = y - startY; 
+        list.scrollTop = scrollTop - walk;
+
+        handleScrollLoop();
+    });
+
+    // Esconder e mostrar o personagens
+    for (const face of faces) {
+        face.addEventListener('click', () => {
+            const id = face.getAttribute('data-character-face');
+            const info = document.querySelector(`[data-character-id="${id}"]`);
+            const overlay = face.querySelector('.characters__main__list__item__overlay');
+
+            hideAllInfo();
+            hideAllOverlay();
+
+            info.classList.add('characters__info__list__item--is-active');
+            overlay.classList.add('characters__main__list__item__overlay--is-active');
+        });
+    }
+});
+
+
+function hideAllInfo() {
+    const infosContainer = document.querySelectorAll('[data-character-id]')
+
+    for (const info of infosContainer) {
+        info.classList.remove('characters__info__list__item--is-active')
+    }
+}
+
+function hideAllOverlay() {
+    const overlayContainer = document.querySelectorAll('.characters__main__list__item__overlay')
+
+    for (const overlay of overlayContainer) {
+        overlay.classList.remove('characters__main__list__item__overlay--is-active')
+    }
+}
