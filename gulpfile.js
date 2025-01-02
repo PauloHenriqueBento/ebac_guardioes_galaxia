27b2a8458fa5df55e4d9c1b2e5275a29244b177c
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2025-01-02 09:21:16 -0300

    style(hero): enhance trailer section for better responsiveness and layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts')
+        );
+}
 
 function images() {
     return gulp.src('./src/images/**/*')
@@ -14,7 +22,7 @@ function styles() {
         .pipe(gulp.dest('./dist/styles'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
